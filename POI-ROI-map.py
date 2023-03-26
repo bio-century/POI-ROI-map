965ba237d0943cd192495cd652e1a1bb2bd1190f
bio-century <info@bio-century.net>
2023-03-26 23:34:35 +0200

    .py comment revision

--- POI-ROI-map.py
+++ POI-ROI-map.py
@@ -1,34 +1,39 @@
-# import Python libraries
+##########################################################################################
+##  (00)                           Preparation Section                                  ##
+##########################################################################################
+
+##########################################################################################
+######  (00.01)                         Import Python Libraries                         ##
+##########################################################################################
 import folium
 import geopandas as gpd
 import pandas as pd
 from folium.plugins import FloatImage
 from folium.plugins import MarkerCluster
 
-
-
+##########################################################################################
+######  (00.02)                        Import Data from Database                        ##
+##########################################################################################
 # preamble before getting started: The initial idea of visualization was obtained from the following sources:
 # https://towardsdatascience.com/mapping-with-matplotlib-pandas-geopandas-and-basemap-in-python-d11b57ab5dac
 # https://python-visualization.github.io/folium/quickstart.html
-data_icons=pd.read_excel('./resources/database.xlsx', index_col=None, header=[0], sheet_name='icons')
-data_ROI_areas=pd.read_excel('./resources/database.xlsx', index_col=None, header=[0], sheet_name='ROI_areas')
-data_dist_cycles=pd.read_excel('./resources/database.xlsx', index_col=None, header=[0], sheet_name='dist_cycles')
-data_starting_point=pd.read_excel('./resources/database.xlsx', index_col=None, header=[0], sheet_name='starting_point')
+PATH_DATABASE = './resources/database.xlsx'
+PATH_LEGEND = './../resources/images/legend.png'
+data_icons=pd.read_excel(PATH_DATABASE, index_col=None, header=[0], sheet_name='icons')
+data_ROI_areas=pd.read_excel(PATH_DATABASE, index_col=None, header=[0], sheet_name='ROI_areas')
+data_dist_cycles=pd.read_excel(PATH_DATABASE, index_col=None, header=[0], sheet_name='dist_cycles')
+data_starting_point=pd.read_excel(PATH_DATABASE, index_col=None, header=[0], sheet_name='starting_point')
 
-
-
-# define startpoint of the geomap view
+##########################################################################################
+######  (00.03)                      Set Startpoint & Initiate Map                      ##
+##########################################################################################
 # https://www.earthdatascience.org/tutorials/introduction-to-leaflet-animated-maps/
 center_of_view = [float(data_starting_point.iloc[0]['latitude']), float(data_starting_point.iloc[0]['longitude'])]
-
-
-
-# initiate map
 m = folium.Map(location=center_of_view, tiles="OpenStreetMap", zoom_start=4.5, control_scale=True)
 
-
-
-# create and fill headline of html doc
+##########################################################################################
+######  (00.04)                           Set Up HTML Header                            ##
+##########################################################################################
 # adapted from:
 # https://stackoverflow.com/questions/33086212/center-3-floating-divs-inside-a-wrapper-div
 # https://code2care.org/pages/how-to-place-two-div-elements-next-to-each-other
@@ -73,6 +78,13 @@ m.get_root().html.add_child(folium.Element(title_html))
 
 
 
+##########################################################################################
+##  (01)                  Set Up And Add Map Features And Marker                        ##
+##########################################################################################
+
+##########################################################################################
+######  (01.01)                   Implement EU Country Borders To Map                   ##
+##########################################################################################
 # implement EU country borders to map
 # https://ec.europa.eu/eurostat/web/gisco/geodata/reference-data/administrative-units-statistical-units/countries#countries20
 marker_cluster_EU_countries = MarkerCluster(name='EU countries',show=True)
@@ -83,9 +95,9 @@ geo_j = folium.GeoJson(data=eu_df,style_function=lambda x: {'fillColor': 'none'}
 geo_j.add_to(marker_cluster_EU_countries)
 marker_cluster_EU_countries.add_to(m)
 
-
-
-# add different standard map representations to the menu
+##########################################################################################
+######  (01.02)              Add Different Map Representations To The Menu              ##
+##########################################################################################
 folium.TileLayer('stamentoner').add_to(m)
 folium.TileLayer('stamenTerrain').add_to(m)
 folium.TileLayer('stamenwatercolor').add_to(m)
@@ -93,8 +105,9 @@ folium.TileLayer('cartodbpositron').add_to(m)
 folium.TileLayer('cartodbdark_matter').add_to(m)
 folium.TileLayer("", name="None", attr="blank").add_to(m)
 
-
-
+##########################################################################################
+######  (01.03)                    Add Cycles Of ROI Regions To Map                     ##
+##########################################################################################
 # draw circles around the center_of_view-position for distance-benchmark depiction
 # coordinates & radii: ROI_map3.xlsx, sheets 'starting_point' & 'Radii'
 def draw_circle(input_radius,input_center_circle,input_color,input_fill):
@@ -107,12 +120,8 @@ def draw_circle(input_radius,input_center_circle,input_color,input_fill):
         #popup = "",
         #opacity=0.5,
     ).add_to(m)
-
 for i in range(0,len(data_dist_cycles)):
     draw_circle(float(data_dist_cycles.iloc[i]['radius']),center_of_view,data_dist_cycles.iloc[i]['color'],False)
-
-
-
 # optional: circles for dedicated regions of interest
 # latitude, longitude & radii: ROI_map3.xlsx, sheets 'ROI_areas'
 def draw_circle2(input_radius,input_center_circle,input_color,input_fill):
@@ -124,20 +133,22 @@ def draw_circle2(input_radius,input_center_circle,input_color,input_fill):
         weight=0.99,
         opacity=0.7,
     ).add_to(m)
-
 for i in range(0,len(data_ROI_areas)):
     draw_circle2(float(data_ROI_areas.iloc[i]['radius']),[float(data_ROI_areas.iloc[i]['latitude']),float(data_ROI_areas.iloc[i]['longitude'])],data_ROI_areas.iloc[i]['color'],True)
-
 lgd_txt = '<span style="color: {col};">{txt}</span>'
 color='red'
 
-
-
+######################################################################################
+##  (01.04)         Cluster Icons (Respecting Location, Category And Zoom)          ##
+######################################################################################
 # cluster icons with respect to location, category and zoom
 marker_cluster_cluster1 = MarkerCluster(name="cluster1")
 marker_cluster_cluster2 = MarkerCluster(name="cluster2")
 marker_cluster_individual_marker = MarkerCluster(name="individual markercluster")
 
+######################################################################################
+##  (01.05)             Load And Add Icon Specifications To Popup Menu              ##
+######################################################################################
 # read in icons properties in a loop
 for i in range(0,len(data_icons)):
     print(str(i))
@@ -172,13 +183,13 @@ for i in range(0,len(data_icons)):
         pl.add_to(marker_cluster_cluster2)
     else:
         pl.add_to(marker_cluster_individual_marker)
-
 marker_cluster_cluster1.add_to(m)
 marker_cluster_cluster2.add_to(m)
 marker_cluster_individual_marker.add_to(m)
 
-
-
+######################################################################################
+##  (01.06)                        Add Layer Control To Map                         ##
+######################################################################################
 # add layer control to the map
 folium.map.LayerControl('topright', collapsed=False).add_to(m)
 
@@ -190,14 +201,16 @@ latitudeslist=[]
 for i in range(0,len(data_icons)):
     latitudeslist.append(float(data_icons.iloc[i]['longitude']))
 
-
-
-# Add legend for icon explanation (25dpi)
-image = ('./../resources/images/legend.png')
+######################################################################################
+##  (01.07)                 Add Legend Specifying Icons And Marker                  ##
+######################################################################################
+image = (PATH_LEGEND)
 FloatImage(image, bottom=7, left=1).add_to(m)
 
 
 
-# save html
+##########################################################################################
+##  (02)                         Save As Interactive HTML                               ##
+##########################################################################################
 m.save("./target/POI-ROI-map.html")
 #m.save("./target/01_MAPS.html")
